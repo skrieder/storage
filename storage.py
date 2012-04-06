3e75a0dcb86af58ee1c4f18e3cf95a3921a41eb5
Scott J. Krieder <skrieder@iit.edu>
2012-04-06 16:08:10 -0500

    all 5 basic tasks working

--- storage.py
+++ storage.py
@@ -4,8 +4,8 @@ import sys
 bucketName = 'skrieder'
 # declare the insert
 def insert(key, value):
-    insertCMD = 'gsutil cp '+ sys.argv[2] + ' ' + 'gs://skrieder'
     try:
+        insertCMD = 'gsutil cp '+ sys.argv[2] + ' ' + 'gs://skrieder'
         os.system(insertCMD)
         return True
     except:
@@ -13,9 +13,25 @@ def insert(key, value):
 
 # declare the delete
 def Remove(key):
-    removeCMD = 'gsutil rm gs://skrieder/' + sys.argv[2]
-    os.system(removeCMD)
     try:
+        removeCMD = 'gsutil rm gs://skrieder/' + sys.argv[2]
+        os.system(removeCMD)
+        return True
+    except:
+        return False
+
+# declare the check
+def Check(key):
+    try:
+        checkCMD = 'gsutil ls gs://skrieder/ > ~/Desktop/storage/output.txt'
+        os.system(checkCMD)
+        ReadFilee = '~/Desktop/storage/output.txt'
+        ins = open( ReadFile, "r" )
+        array = []
+        print("in check")
+        for line in ins:
+            array.append( line )
+        print(array)
         return True
     except:
         return False
@@ -38,6 +54,8 @@ if(len(sys.argv) > 1):
         manual()
     elif(sys.argv[1] == 'remove'):
         Remove(sys.argv[2])
+    elif(sys.argv[1] == 'check'):
+        Check(sys.argv[2])
 else:
     print('python storage.py "command"')
     print('type python storage.py help for the manual')
